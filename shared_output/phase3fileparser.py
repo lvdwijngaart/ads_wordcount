964c8529f15e4232395457f9f8d4164be0071d50
peppidesu <bakker.pepijn@gmail.com>
2025-10-22 12:13:53 +0200

    timestamps

--- shared_output/phase3fileparser.py
+++ shared_output/phase3fileparser.py
@@ -1,28 +1,34 @@
+from datetime import datetime
+from math import ceil
 import numpy as np
 import matplotlib.pyplot as plt
+
 filepath = "shared_output/latencies.txt"
+
+
 # Read all lines
 with open(filepath, "r") as f:
     lines = f.readlines()
-data  = list(  [[] for _ in range(20)]  )  # 20 clients
 
+timestamps = np.zeros(len(lines))
+latencies = np.zeros(len(lines))
+origin = None
 for idx, line in enumerate(lines):
-
-    latency = float(line.strip())
-    data[idx % 20].append(latency)
-
-
-averages = [sum(client_data) / len(client_data) if client_data else 0 for client_data in data]
-print(averages)
+    parts = line.strip().split(",")
+    timestamp = datetime.fromisoformat(parts[0])
+    latency = float(parts[1])
+    if not origin:
+        origin = timestamp
+    time = (timestamp - origin).total_seconds()
+    timestamps[idx] = time
+    latencies[idx] = latency
 
 plt.figure(figsize=(12, 6))
-plt.bar(range(1, len(averages) + 1), averages)
-plt.xlabel('Request Index')
-plt.ylabel('Average Latency (ms)')
-plt.title('Average Latency of Request 1 to 20')
-plt.xticks(range(1, len(averages) + 1))
-plt.grid(axis='y', alpha=0.3)
+plt.scatter(timestamps, latencies)
+plt.xlabel("Time")
+plt.ylabel("Latency (ms)")
+plt.title("Average Latency of Request 1 to 20")
+# plt.xticks(range(1, len(averages) + 1))
+plt.grid(axis="y", alpha=0.3)
 plt.tight_layout()
 plt.show()
-
-
